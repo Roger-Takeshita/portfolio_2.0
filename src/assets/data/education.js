75fefe460533cfb8a84feeb245c1f61a320e90fd
Roger Takeshita <roger.takeshita@gmail.com>
2020-09-12 01:08:25 -0400

    update resume, add new certificate

--- src/assets/data/education.js
+++ src/assets/data/education.js
@@ -3,6 +3,7 @@ import reactNativeCert from '../images/certificates/certificate_002.jpg';
 import typeScriptCert from '../images/certificates/certificate_003.jpg';
 import nodejsCert from '../images/certificates/certificate_004.jpg';
 import dataStructureCert from '../images/certificates/certificate_005.jpg';
+import cssCert from '../images/certificates/certificate_006.jpg';
 import gaLogo from '../images/logos/education/education_ga.png';
 import pucLogo from '../images/logos/education/education_puc.png';
 
@@ -40,6 +41,10 @@ export const onlineEducation = [
     {
         certificateUri: typeScriptCert,
         alt: 'TypeScript Certificate',
+    },
+    {
+        certificateUri: cssCert,
+        alt: 'Advanced CSS and SASS',
     },
     {
         certificateUri: nodejsCert,
